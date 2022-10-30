441a528bb16aa5910ff74f8dd62eff89a29ddc7b
Matheus Ferreira <matheus.ferreira@dotgroup.com.br>
2022-10-30 18:39:03 -0300

    teste

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { default: axios } = require('axios');
-
+var vezes = 0;
 const logData = async () => {
-	const apuracoes = await axios("https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json")
+	const apuracoes = await axios("https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json")
 		.then(res => res.data.cand.map(cand => ({ name: cand.nm, votos: cand.vap, porcentagem: cand.pvap })));
 	console.table(apuracoes);
-	setTimeout(logData, 60000 * 2);
+	const bahia = await axios("https://resultados.tse.jus.br/oficial/ele2022/547/dados-simplificados/ba/ba-c0003-e000547-r.json")
+	.then(res => res.data.cand.map(cand => ({ name: cand.nm, votos: cand.vap, porcentagem: cand.pvap })));
+	console.table(bahia);
+	console.log(vezes++);
+	setTimeout(logData, 10000 * 2);
 } 
 
 logData();
